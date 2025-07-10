fa4de448f56c167c5d1b3eb80fef46fbb135f797
sulthana-parveen <76679665+sulthana-parveen@users.noreply.github.com>
2025-07-10 13:50:58 +0530

    globalStyles

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,5 +1,6 @@
 import { StyleSheet, Dimensions } from 'react-native';
 
+
 const { width,height } = Dimensions.get('window');
 
 export const globalStyles = StyleSheet.create({
@@ -14,6 +15,11 @@ export const globalStyles = StyleSheet.create({
     width: width * 0.85,
     alignSelf: 'center',
   },
+   formWrapper1:{
+    margin:height*0.08,
+    width: width * 0.85,
+    alignSelf: 'center',
+  },
   bottomTextView:{
     marginTop:height*0.04,
     alignSelf:'center'
@@ -25,11 +31,40 @@ export const globalStyles = StyleSheet.create({
     marginTop:height*0.01
   },
   button: {
-    width: '100%',
-    paddingVertical: 8,
+    width: width*0.5,
+    paddingVertical: height*0.007,
     marginTop: 20,
+    alignSelf:'center'
   },
   buttonText:{
     fontSize:width*0.045
-  }
+  },
+  welcomeText:{
+    alignSelf:'center',
+    fontSize:width*0.065,
+    fontWeight:'600'
+  },
+  subText:{
+    fontSize:width*0.05,
+    marginVertical:height*0.015
+  },
+  countryText:{
+    alignSelf:'center',
+    fontSize:width*0.05,
+    marginVertical:height*0.015
+  },
+  userText:{
+    fontSize:width*0.04,
+    marginVertical:height*0.01
+  },
+  input: {
+    width:width*0.85,
+    marginBottom:height*0.03,
+  },
+ title: {
+    fontSize: 28,
+    fontWeight: '600',
+    marginBottom: 24,
+    textAlign: 'center',
+  },
 });
