8743812563a9d3a58410e162e949ecad7c1cfddc
sulthana-parveen <76679665+sulthana-parveen@users.noreply.github.com>
2025-07-10 13:50:16 +0530

    navigation

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -4,15 +4,20 @@ import { createStackNavigator } from '@react-navigation/stack'
 import RegisterScreen from '../screens/RegisterScreen'
 import LoginScreen from '../screens/LoginScreen'
 import { NavigationContainer } from '@react-navigation/native'
+import DashBoard from '../screens/DashBoard'
+import AdminDashBoard from '../screens/AdminDashBoard'
 const Stack = createStackNavigator()
-const AppNavigation :React.FC= () => {
+const AppNavigation: React.FC = () => {
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name='LoginScreen'component={LoginScreen}/>
-      <Stack.Screen name='RegisterScreen' component={RegisterScreen}/>
-      
-    </Stack.Navigator>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name='LoginScreen' component={LoginScreen} />
+        <Stack.Screen name='RegisterScreen' component={RegisterScreen} />
+        <Stack.Screen name='DashBoard' component={DashBoard} />
+        <Stack.Screen name='AdminDashBoard' component={AdminDashBoard} />
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
