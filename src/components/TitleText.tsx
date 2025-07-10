1e58c586a7bd82de6bca88affd3f040ce7b3def5
sulthana-parveen <76679665+sulthana-parveen@users.noreply.github.com>
2025-07-10 13:35:13 +0530

    titletext

--- src/components/TitleText.tsx
+++ src/components/TitleText.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
+import { globalStyles } from '../styles/styles';
 
 interface TitleTextProps {
   text: string;
 }
 
 const TitleText: React.FC<TitleTextProps> = ({ text }) => {
-  return <Text style={styles.title}>{text}</Text>;
-};
-
+  return <Text
+    style={globalStyles.title} >
+    {text}
+  </Text>
+}
 export default TitleText;
 
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 28,
-    fontWeight: '600',
-    marginBottom: 24,
-    textAlign: 'center',
-  },
-});
